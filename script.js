031f1353eca2497b85f498e37b9474a28afc4e41
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2024-05-29 10:11:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,14 +42,15 @@ function toggleWindow(windowId, frameId) {
 function fullscreen(id) {
     const windowElement = document.getElementById(id);
     const iframe = windowElement.querySelector('iframe');
+    const taskbar = document.getElementById("taskbar");
     
     if (windowElement.classList.contains('fullscreen')) {
         windowElement.classList.remove('fullscreen');
         iframe.classList.remove('fullscreen');
-        $('#taskbar').show();
+        taskbar.style.display = "flex";
     } else {
         windowElement.classList.add('fullscreen');
         iframe.classList.add('fullscreen');
-        $('#taskbar').hide();
+        taskbar.style.display = "none";
     }
 }
