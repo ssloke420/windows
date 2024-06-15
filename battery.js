ae445f4ac7fe712855b827b308424cb5312daa4d
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2024-06-15 09:33:53 -0400

    Update battery.js

--- battery.js
+++ battery.js
@@ -1,11 +1,11 @@
-//battery
-async function updateBattery() {
-    const battery = await navigator.getBattery();
-    document.getElementById('battery').innerText = `Battery: ${Math.round(battery.level * 100)}%`;
+// //battery
+// async function updateBattery() {
+//     const battery = await navigator.getBattery();
+//     document.getElementById('battery').innerText = `Battery: ${Math.round(battery.level * 100)}%`;
 
-    // Update battery status on change
-    battery.onlevelchange = () => {
-        document.getElementById('battery').innerText = `Battery: ${Math.round(battery.level * 100)}%`;
-    };
-}
-updateBattery();
+//     // Update battery status on change
+//     battery.onlevelchange = () => {
+//         document.getElementById('battery').innerText = `Battery: ${Math.round(battery.level * 100)}%`;
+//     };
+// }
+// updateBattery();
