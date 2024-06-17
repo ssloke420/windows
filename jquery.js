e7f8aba14418504ab4dca08cca2c5ec3780d176f
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2024-06-17 13:45:58 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    // Attach a click event handler to the element with id 'start'
-    $('#start').click(function() {
-        // Show a confirmation dialog
-        var confirmation = confirm("Are you sure you want to shutdown?");
-        if (confirmation) {
-            // If confirmed, remove the background image and empty the body content
-            $('body').css('background-image', 'none').empty();
-            // Set the background color of the html element to black
-            $('html').css('background-color', 'black');
-        }
-    });
+// $(document).ready(function() {
+//     // Attach a click event handler to the element with id 'start'
+//     $('#start').click(function() {
+//         // Show a confirmation dialog
+//         var confirmation = confirm("Are you sure you want to shutdown?");
+//         if (confirmation) {
+//             // If confirmed, remove the background image and empty the body content
+//             $('body').css('background-image', 'none').empty();
+//             // Set the background color of the html element to black
+//             $('html').css('background-color', 'black');
+//         }
+//     });
 
-    // Attach a double-click event handler to elements with the class 'frameshown'
-    $('.frameshown').dblclick(function() {
-        var id = $(this).attr('id'); // Get the id of the element
-        fullscreen(id); // Call the fullscreen function with the id as parameter
-    });
-});
+//     // Attach a double-click event handler to elements with the class 'frameshown'
+//     $('.frameshown').dblclick(function() {
+//         var id = $(this).attr('id'); // Get the id of the element
+//         fullscreen(id); // Call the fullscreen function with the id as parameter
+//     });
+// });
